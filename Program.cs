6e2371cccbb0df35b683c3052c45633d4b8a3f90
DHANUSH M.P <106876820+dhanushmp@users.noreply.github.com>
2024-09-11 00:03:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using WebApplication2;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR().AddAzureSignalR();
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseRouting();
+app.UseStaticFiles();
+app.MapHub<ChatSampleHub>("/chat");
+app.Run();
